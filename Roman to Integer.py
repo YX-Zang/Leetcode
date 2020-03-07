969b143d5a4d29eccff1b3d5b8e31758e4ddcacd
Xiang_Z <s202140517@gmail.com>
2020-03-07 22:55:26 +0800

    update

--- Roman to Integer.py
+++ Roman to Integer.py
@@ -4,23 +4,12 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        dic = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
-        }
+        dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         count = 0
         list1 = [dic[string] for string in s]
-
-        for index in range(len(list1)):
-            if index + 1 != len(list1):
-                flag = 1 if list1[index] >= list1[index + 1] else -1
-                list1[index] = flag * list1[index]
-
-        for i in list1:
-            count += i
+        for index in range(len(list1)-1):
+            if list1[index] < list1[index+1]:
+                list1[index] *= -1
+            count+= list1[index]
+        count += list1[len(list1)-1]
         return count

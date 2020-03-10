afde0681f5caaaf02ced9dfc0730a14178dc6778
YX-Zang <34359549+YX-Zang@users.noreply.github.com>
2020-03-10 18:02:43 +0800

    init

---
+++ Letter Combinations of a Phone Number.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        if not digits:
+            return []
+        else:
+            d = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
+            res = ['']
+            for e in digits:
+                res = [w + c for c in d[e] for w in res]
+            return res

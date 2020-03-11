eb6a83c6fae899fe24161cbbbd130a9524921d74
YX-Zang <34359549+YX-Zang@users.noreply.github.com>
2020-03-11 16:53:19 +0800

    init

--- Excel Sheet Column Number.py
+++ Excel Sheet Column Number.py
@@ -9,11 +9,3 @@ class Solution(object):
             asc = ord(string) - 64
             count = count + asc * 26 ** index
         return count
-
-if __name__ == '__main__':
-    # a = ord('A') - 64
-    # b = ord('Z') - 64
-    # print(a)
-    # print(b)
-    Solution = Solution()
-    print(Solution.titleToNumber('ZY'))

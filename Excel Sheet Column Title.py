95a3605b7dc39303a0ca5e97a5bf956ccbe85f32
YX-Zang <34359549+YX-Zang@users.noreply.github.com>
2020-03-12 14:28:31 +0800

    update

--- Excel Sheet Column Title.py
+++ Excel Sheet Column Title.py
@@ -7,6 +7,6 @@ class Solution(object):
         string = ''
         while n:
             string = chr(int((n - 1) % 26) + 65) + string
-            n = (n - 1) / 26
+            n = (n - 1) // 26
         return string
 

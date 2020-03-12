a721e00661a0201faff81bb4a41cb538e44e479c
YX-Zang <34359549+YX-Zang@users.noreply.github.com>
2020-03-12 12:20:35 +0800

    init

--- Excel Sheet Column Title.py
+++ Excel Sheet Column Title.py
@@ -5,20 +5,8 @@ class Solution(object):
         :rtype: str
         """
         string = ''
-        while True:
-            if n > 26:
-                sqr = n//26
-                n = n - sqr * 26
-                string += chr(sqr + 64)
-            elif n == 26:
-                string += chr(n % 27 + 64)
-                break
-            else:
-                string += chr(n % 26 + 64)
-                break
+        while n:
+            string = chr(int((n - 1) % 26) + 65) + string
+            n = (n - 1) / 26
         return string
 
-if __name__ == '__main__':
-    Solution = Solution()
-    print(Solution.convertToTitle(52))
-    # print(28//26)

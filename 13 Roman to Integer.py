46dbeb9a8ef78e28698c4e3dc9310df14dad92be
YX-Zang <34359549+YX-Zang@users.noreply.github.com>
2020-03-19 17:58:55 +0800

    update

--- 13 Roman to Integer.py
+++ 13 Roman to Integer.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'X': 10,'C': 100, 'D': 500, 'M': 1000}
+        dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         ls1 = [dic[i] for i in s]
         for i in range(len(ls1)-1, 0, -1):
             if ls1[i] > ls1[i-1]:

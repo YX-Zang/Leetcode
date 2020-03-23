02bd30b2c56036b3977e587d68ae94d94fd3a246
YX-Zang <34359549+YX-Zang@users.noreply.github.com>
2020-03-23 14:40:19 +0800

    init

---
+++ 345 Reverse Vowels of a String.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def reverseVowels(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        ls1 = []
+        for index, v in enumerate(s):
+            if v.lower() in ['a', 'e', 'i', 'o', 'u']:
+                ls1.append(index)
+        ls1_rev = ls1[::-1]
+        s2 = list(s)
+        for i in range(len(ls1_rev)):
+            s2[ls1_rev[i]] = s[ls1[i]]
+        return ''.join(s2)

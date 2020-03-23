9f2e2c081ea402e085467dc9272effbdb0668386
YX-Zang <34359549+YX-Zang@users.noreply.github.com>
2020-03-23 18:14:43 +0800

    update optimization

--- 367 Valid Perfect Square.py
+++ 367 Valid Perfect Square.py
@@ -4,6 +4,5 @@ class Solution(object):
         :type num: int
         :rtype: bool
         """
-        sqr = '%d' % num ** 0.5
-        return True if int(sqr)**2 == num else False
+        return (num ** 0.5).is_integer()
 

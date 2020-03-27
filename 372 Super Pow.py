2ce4653a542880379798fca3361b9dab51688915
YX-Zang <34359549+YX-Zang@users.noreply.github.com>
2020-03-27 14:41:52 +0800

    init

---
+++ 372 Super Pow.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def superPow(self, a, b):
+        """
+        :type a: int
+        :type b: List[int]
+        :rtype: int
+        """
+        s = ''.join(str(i) for i in b)
+        s1 = int(s)
+        return pow(a , s1 , 1337)

73b36ab2f865524d7c2faf17d3fd5ff51cf00777
YX-Zang <34359549+YX-Zang@users.noreply.github.com>
2020-03-31 12:53:10 +0800

    init

---
+++ 949 Largest Time for Given Digits.py
@@ -0,0 +1,14 @@
+import itertools
+class Solution(object):
+    def largestTimeFromDigits(self, A):
+        """
+        :type A: List[int]
+        :rtype: str
+        """
+        ls1 = []
+        for i in itertools.permutations(A):
+            hour = i[0]*10 + i[1]
+            minute = i[2]*10 + i[3]
+            if hour < 24 and minute < 60:
+                ls1.append((hour, minute))
+        return '%02d:%02d' % (max(ls1)[0], max(ls1)[1]) if ls1 else ''

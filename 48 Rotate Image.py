c2ccf8ca2e1831b081b04ac73ee15fcdb2d5a528
YX-Zang <34359549+YX-Zang@users.noreply.github.com>
2020-04-07 14:12:55 +0800

    init

--- 48 Rotate Image.py
+++ 48 Rotate Image.py
@@ -4,8 +4,13 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
+        # Type I
         length = len(matrix[0])
         ls0 = []
         for i in matrix:
             ls0 += i
         matrix[:] = [ls0[i::length][::-1] for i in range(length)]
+        return matrix
+        matrix[:] = [[i for i in matrix]]
+        # Type II
+        # matrix[:] = [[i[j] for i in matrix[::-1]] for j in range(len(matrix[0]))]
